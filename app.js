f5b4ab88547e4cbf874182540c232938bc7f374c
Dan Tkachuk <daniil.tkachuk@gmail.com>
2021-09-16 00:40:08 +0300

    Added dark theme

--- app.js
+++ app.js
@@ -3,16 +3,25 @@
 window.addEventListener('DOMContentLoaded', () => {
 
   const hoursElem = document.querySelector('#hours'),
-        minutesElem = document.querySelector('#minutes'),
-        secondsElem = document.querySelector('#seconds');
+    minutesElem = document.querySelector('#minutes'),
+    secondsElem = document.querySelector('#seconds'),
+    darkBtn = document.querySelector('#dark'),
+    cards = document.querySelectorAll('.card'),
+    containers = document.querySelectorAll('.container');
   let now = new Date(),
-      hours = now.getHours(),
-      minutes = now.getMinutes(),
-      seconds = now.getSeconds();
+    hours = now.getHours(),
+    minutes = now.getMinutes(),
+    seconds = now.getSeconds(),
+    isDark = false;
+
   if (seconds < 10) {
     seconds = `0${seconds}`;
-  } else if (minutes < 10) {
+  }
+  if (minutes < 10) {
     minutes = `0${minutes}`;
+  }
+  if (hours < 10) {
+    hours = `0${hours}`;
   }
 
   hoursElem.textContent = hours;
@@ -27,13 +36,37 @@ window.addEventListener('DOMContentLoaded', () => {
 
     if (seconds < 10) {
       seconds = `0${seconds}`;
-    } else if (minutes < 10) {
+    }
+    if (minutes < 10) {
       minutes = `0${minutes}`;
     }
-    
+    if (hours < 10) {
+      hours = `0${hours}`;
+    }
+
     hoursElem.textContent = `${hours}`;
     minutesElem.textContent = `${minutes}`;
     secondsElem.textContent = `${seconds}`;
   }, 1000);
+
+  darkBtn.addEventListener('click', changeTheme);
+
+  function changeTheme() {
+    darkBtn.classList.toggle('dark');
+    if (isDark) {
+      darkBtn.textContent = 'Dark';
+      isDark = false;
+    } else {
+      darkBtn.textContent = 'Light';
+      isDark = true;
+    }
+    cards.forEach(card => {
+      card.classList.toggle('dark');
+    });
+    containers.forEach(container => {
+      container.classList.toggle('dark');
+    });
+    document.body.classList.toggle('dark');
+  }
 
 });
