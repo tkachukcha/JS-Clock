e731b0f72d75f5fb5598ecc3156549d2cb2a65da
Dan Tkachuk <daniil.tkachuk@gmail.com>
2021-09-20 01:01:32 +0300

    Cleaned code, started analog clock

--- app.js
+++ app.js
@@ -2,72 +2,59 @@
 
 window.addEventListener('DOMContentLoaded', () => {
 
-  const hoursElem = document.querySelector('#hours'),
+  const
+    hoursElem = document.querySelector('#hours'),
     minutesElem = document.querySelector('#minutes'),
     secondsElem = document.querySelector('#seconds'),
-    darkBtn = document.querySelector('#dark'),
+    amPmElem = document.querySelector('.ampm'),
+
+    themeBtn = document.querySelector('#dark'),
     pmBtn = document.querySelector('#pm'),
+    clockTypeBtn = document.querySelector('#clockType'),
+
     cards = document.querySelectorAll('.card'),
-    containers = document.querySelectorAll('.container'),
-    modeTxts = document.querySelectorAll('span'),
+    containers = document.querySelectorAll('.clock-body'),
+    optionNames = document.querySelectorAll('.option-name'),
     switchers = document.querySelectorAll('.switch');
 
-  
-  let now = new Date(),
-    hours = now.getHours(),
-    minutes = now.getMinutes(),
-    seconds = now.getSeconds();
+  let
+    now,
+    hours,
+    minutes,
+    seconds;
 
-  
-    if (localStorage.getItem('isDark') === null) {
-      localStorage.setItem('isDark', 'false');
-    } else if (localStorage.getItem('isDark') === 'true') {
-    darkBtn.classList.toggle('on');
+  showTime();
 
-      darkThemeOn();
-    }
+  const secondsTick = setInterval(() => {
+    showTime();
+  }, 1000);
 
-  if (localStorage.getItem('isPM') === null) {
-    localStorage.setItem('isPM', 'false');
-  } else if (localStorage.getItem('isPM') === 'true') {
-    pmBtn.classList.toggle('on');
-    modeTxts[1].textContent = 'AM/PM Mode';
-  }
-
-  pmMode();
-  addZeroBefore();
-
-  hoursElem.innerHTML = hours;
-  minutesElem.textContent = minutes;
-  secondsElem.textContent = seconds;
-
-  setInterval(() => {
+  function showTime() {
     now = new Date();
     hours = now.getHours();
     minutes = now.getMinutes();
     seconds = now.getSeconds();
 
-    pmMode();
+    turnOnPMMode();
     addZeroBefore();
 
-    hoursElem.innerHTML = `${hours}`;
-    minutesElem.textContent = `${minutes}`;
-    secondsElem.textContent = `${seconds}`;
-  }, 1000);
+    hoursElem.innerHTML = hours;
+    minutesElem.textContent = minutes;
+    secondsElem.textContent = seconds;
+  }
 
-  darkBtn.addEventListener('click', changeTheme);
-  pmBtn.addEventListener('click', pmModeChange);
-
-  function pmMode() {
+  function turnOnPMMode() {
     if (localStorage.getItem('isPM') === 'true') {
-      if (hours === 0) {
-        hours = '12<span class="ampm">am</span>';
-      } else if (hours === 12) {
-        hours += '<span class="ampm">pm</span>';
+      if (hours === 12) {
+        amPmElem.textContent = 'pm';
+      } else if (hours === 0) {
+        hours = 12;
+        amPmElem.textContent = 'am';
       } else if (hours < 12) {
-        hours += '<span class="ampm">am</span>';
+        amPmElem.textContent = 'am';
       } else if (hours > 12) {
-        hours = hours - 12 + '<span class="ampm">pm</span>';
+        hours = hours - 12;
+        amPmElem.textContent = 'pm';
       }
     }
   }
@@ -79,68 +66,79 @@ window.addEventListener('DOMContentLoaded', () => {
     if (minutes < 10) {
       minutes = `0${minutes}`;
     }
-    if (hours < 10) {
-      hours = `0${hours}`;
+    if (hours === 0) {
+      hours = '00';
     }
+  }
+
+  initTheme();
+  initPMMode();
+
+  function initTheme() {
+    if (localStorage.getItem('isDark') === null) {
+      localStorage.setItem('isDark', 'false');
+    } else if (localStorage.getItem('isDark') === 'true') {
+      optionNames[0].textContent = 'Dark Mode';
+      toggleTheme();
+    }
+  }
+
+  function initPMMode() {
+    if (localStorage.getItem('isPM') === null) {
+      localStorage.setItem('isPM', 'false');
+    } else if (localStorage.getItem('isPM') === 'true') {
+      pmBtn.classList.toggle('on');
+      optionNames[1].textContent = 'AM/PM Mode';
+      amPmElem.classList.toggle('hide');
+    }
+  }
+
+  // Options buttons
+
+  themeBtn.addEventListener('click', changeTheme);
+  pmBtn.addEventListener('click', changePMmode);
+
+  function changePMmode() {
+    pmBtn.classList.toggle('on');
+    amPmElem.classList.toggle('hide');
+    if (localStorage.getItem('isPM') === 'true') {
+      optionNames[1].textContent = '24H Mode';
+      localStorage.setItem('isPM', false);
+    } else {
+      optionNames[1].textContent = 'AM/PM Mode';
+      localStorage.setItem('isPM', true);
+    }
+    showTime();
   }
 
   function changeTheme() {
-    darkBtn.classList.toggle('on');
-    
     if (localStorage.getItem('isDark') === 'true') {
-      modeTxts[0].textContent = 'Light Mode';
-      localStorage.setItem('isDark','false');
-      darkThemeOff();
+      localStorage.setItem('isDark', 'false');
+      optionNames[0].textContent = 'Light Mode';
+      toggleTheme();
     } else if (localStorage.getItem('isDark') === 'false') {
-      modeTxts[0].textContent = 'Dark Mode';
-      
-      localStorage.setItem('isDark','true');
-      darkThemeOn();
-    }
-    
-  }
-
-  function pmModeChange() {
-    pmBtn.classList.toggle('on');
-    if (localStorage.getItem('isPM') === 'true') {
-      modeTxts[1].textContent = '24H Mode';
-      localStorage.setItem('isPM', false);
-    } else {
-      modeTxts[1].textContent = 'AM/PM Mode';
-      localStorage.setItem('isPM', true);
+      localStorage.setItem('isDark', 'true');
+      optionNames[0].textContent = 'Dark Mode';
+      toggleTheme();
     }
   }
 
-  function darkThemeOn() {
+  function toggleTheme() {
+    themeBtn.classList.toggle('on');
+    amPmElem.classList.toggle('dark');
     cards.forEach(card => {
-      card.classList.add('dark');
+      card.classList.toggle('dark');
     });
     containers.forEach(container => {
-      container.classList.add('dark');
+      container.classList.toggle('dark');
     });
-    modeTxts.forEach(modeTxt => {
-      modeTxt.classList.add('dark');
-    });
-    switchers.forEach(switcher => {
-      switcher.classList.add('dark');
-    });
-    document.body.classList.add('dark');
-  }
-
-  function darkThemeOff() {
-    cards.forEach(card => {
-      card.classList.remove('dark');
-    });
-    containers.forEach(container => {
-      container.classList.remove('dark');
-    });
-    modeTxts.forEach(modeTxt => {
-      modeTxt.classList.remove('dark');
+    optionNames.forEach(modeTxt => {
+      modeTxt.classList.toggle('dark');
     });
     switchers.forEach(switcher => {
-      switcher.classList.remove('dark');
+      switcher.classList.toggle('dark');
     });
-    document.body.classList.remove('dark');
+    document.body.classList.toggle('dark');
   }
 
 });
