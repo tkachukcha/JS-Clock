0b092b38cfa7f68c75c1e1557a76aa6edb13c7e1
Dan Tkachuk <daniil.tkachuk@gmail.com>
2021-09-20 03:05:12 +0300

    Added analog clock face and seconds arrow

--- app.js
+++ app.js
@@ -2,6 +2,30 @@
 
 window.addEventListener('DOMContentLoaded', () => {
 
+  class Fraction {
+    constructor(angle, translate, scale, name, element) {
+      this.angle = angle;
+      this.translate = translate;
+      this.scale = scale;
+      this.name = name;
+      this.element = element;
+    }
+    render() {
+      const fractionDiv = document.createElement('div');
+      fractionDiv.classList.add('fraction');
+      analogClock.append(fractionDiv);
+      fractionDiv.style.transform = `rotate(${this.angle}deg) translateY(${this.translate}px) scaleY(${this.scale})`;
+      if (this.name !== null) {
+        fractionDiv.id = this.name;
+        this.element = document.querySelector(`#${this.name}`);
+      }
+    }
+
+  }
+
+
+
+
   const
     hoursElem = document.querySelector('#hours'),
     minutesElem = document.querySelector('#minutes'),
@@ -12,16 +36,36 @@ window.addEventListener('DOMContentLoaded', () => {
     pmBtn = document.querySelector('#pm'),
     clockTypeBtn = document.querySelector('#clockType'),
 
+    analogClock = document.querySelector('#analogClock'),
+    digitalClock = document.querySelector('#digitalClock'),
     cards = document.querySelectorAll('.card'),
-    containers = document.querySelectorAll('.clock-body'),
-    optionNames = document.querySelectorAll('.option-name'),
-    switchers = document.querySelectorAll('.switch');
+    switchers = document.querySelectorAll('.switch'),
+    options = document.querySelectorAll('.option'),
+    optionNames = document.querySelectorAll('.option-name');
 
   let
     now,
     hours,
     minutes,
-    seconds;
+    seconds,
+    milliseconds,
+    fractions = [];
+
+  const secondsArrow = new Fraction(0, 0, 0.85, 'secondsArrow');
+  secondsArrow.render();
+
+  renderFractions();
+
+  function renderFractions() {
+    for (let i = 0; i < 120; i += 1) {
+      if (i % 2 === 0) {
+        fractions.push(new Fraction(i * 3, 137, 0.08));
+      } else {
+        fractions.push(new Fraction(i * 3, 143, 0.04));
+      }
+      fractions[i].render();
+    }
+  }
 
   showTime();
 
@@ -30,17 +74,22 @@ window.addEventListener('DOMContentLoaded', () => {
   }, 1000);
 
   function showTime() {
-    now = new Date();
-    hours = now.getHours();
-    minutes = now.getMinutes();
-    seconds = now.getSeconds();
-
+    getTime();
     turnOnPMMode();
     addZeroBefore();
 
     hoursElem.innerHTML = hours;
     minutesElem.textContent = minutes;
     secondsElem.textContent = seconds;
+    secondsArrow.element.style.transform = `rotate(${(seconds*6)+180}deg) translateY(${secondsArrow.translate}px) scaleY(${secondsArrow.scale})`;
+  }
+
+  function getTime() {
+    now = new Date();
+    hours = now.getHours();
+    minutes = now.getMinutes();
+    seconds = now.getSeconds();
+    milliseconds = now.getMilliseconds();
   }
 
   function turnOnPMMode() {
@@ -73,6 +122,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
   initTheme();
   initPMMode();
+  initClockType();
 
   function initTheme() {
     if (localStorage.getItem('isDark') === null) {
@@ -88,8 +138,17 @@ window.addEventListener('DOMContentLoaded', () => {
       localStorage.setItem('isPM', 'false');
     } else if (localStorage.getItem('isPM') === 'true') {
       pmBtn.classList.toggle('on');
-      optionNames[1].textContent = 'AM/PM Mode';
+      optionNames[2].textContent = 'AM/PM Mode';
       amPmElem.classList.toggle('hide');
+    }
+  }
+
+  function initClockType() {
+    if (localStorage.getItem('isAnalog') === null) {
+      localStorage.setItem('isAnalog', 'false');
+    } else if (localStorage.getItem('isAnalog') === 'true') {
+      clockTypeBtn.classList.toggle('on');
+      showAnalogClock();
     }
   }
 
@@ -97,15 +156,16 @@ window.addEventListener('DOMContentLoaded', () => {
 
   themeBtn.addEventListener('click', changeTheme);
   pmBtn.addEventListener('click', changePMmode);
+  clockTypeBtn.addEventListener('click', changeClockType);
 
   function changePMmode() {
     pmBtn.classList.toggle('on');
     amPmElem.classList.toggle('hide');
     if (localStorage.getItem('isPM') === 'true') {
-      optionNames[1].textContent = '24H Mode';
+      optionNames[2].textContent = '24H Mode';
       localStorage.setItem('isPM', false);
     } else {
-      optionNames[1].textContent = 'AM/PM Mode';
+      optionNames[2].textContent = 'AM/PM Mode';
       localStorage.setItem('isPM', true);
     }
     showTime();
@@ -124,13 +184,12 @@ window.addEventListener('DOMContentLoaded', () => {
   }
 
   function toggleTheme() {
+    document.body.classList.toggle('dark');
     themeBtn.classList.toggle('on');
     amPmElem.classList.toggle('dark');
+    digitalClock.classList.toggle('dark');
     cards.forEach(card => {
       card.classList.toggle('dark');
-    });
-    containers.forEach(container => {
-      container.classList.toggle('dark');
     });
     optionNames.forEach(modeTxt => {
       modeTxt.classList.toggle('dark');
@@ -138,7 +197,33 @@ window.addEventListener('DOMContentLoaded', () => {
     switchers.forEach(switcher => {
       switcher.classList.toggle('dark');
     });
-    document.body.classList.toggle('dark');
+
+  }
+
+  function changeClockType() {
+    clockTypeBtn.classList.toggle('on');
+    if (localStorage.getItem('isAnalog') === 'true') {
+      localStorage.setItem('isAnalog', 'false');
+      showDigitalClock();
+    } else if (localStorage.getItem('isAnalog') === 'false') {
+      localStorage.setItem('isAnalog', 'true');
+      showAnalogClock();
+    }
+  }
+
+  function showAnalogClock() {
+    optionNames[1].textContent = 'Analog';
+    digitalClock.classList.add('hide');
+    analogClock.classList.remove('hide');
+    options[2].classList.add('hide');
+
+  }
+
+  function showDigitalClock() {
+    optionNames[1].textContent = 'Digital';
+    digitalClock.classList.remove('hide');
+    analogClock.classList.add('hide');
+    options[2].classList.remove('hide');
   }
 
 });
